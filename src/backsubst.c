13e0119c2ca7a03929329246f8b333d80fc68a6a
bankowj1 <bania1414@gmail.com>
2020-12-30 14:28:51 +0100

    backsub zmiany

--- src/backsubst.c
+++ src/backsubst.c
@@ -5,17 +5,34 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
+	int ir=(mat->r);
+	int ic=(mat->c);
+	double rwn;
+	double rwnpb;
+	if(ic > ir)
+	return 2;
+	for(ir ; ir > 0 ;ir--)
+	{	
+		for(int j = 1 ; j < (mat->c) ;j++)
+		{
+			rwnpb+=(mat->data[ir][ic+j]) * (x->data[ir+j][0]);	
+		}
+		rwn= b->data[ir][0] - rwnpb;
+			if((mat->data[ir][ic]-0)>=1e-6)
+			{	return 1;}
+		x->data[ir][0]=(rwn/(mat->data[ir][ic]));	
+		ic--;
+	}/**
 				 * Tutaj należy umieścić właściwą implemntację.
 				 */
 
 				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
-
+/*
 				int i;
 				for (i =0; i < x->r; i++) {
 								x->data[i][0] = b->data[i][0];
 				}
-
+*/
 				return 0;
 }
 
