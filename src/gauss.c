bdc7688307d1766c93c68ce153b7f58bb872fe01
bankowj1 <bania1414@gmail.com>
2020-12-31 14:47:45 +0100

    sprawdzenie czy macierz osobliwa w(gauss)

--- src/gauss.c
+++ src/gauss.c
@@ -1,11 +1,12 @@
 #include "gauss.h"
-
+#include <math.h>
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    for(int k = 0; k < mat->c-1; k++){
+	int x=0;
+	for(int k = 0; k < mat->c-1; k++){
 	
 		int max = k;
 		for(int i= k; i < ((mat->c)-1); i++){
@@ -23,8 +24,11 @@ int eliminate(Matrix *mat, Matrix *b){
 				(b->data[max][0])=(b->data[k][0]);
 				(b->data[k][0])=tmp;
 		}
-
+	
         for(int w = k+1; w < mat->r; w++){
+		if((fabs(mat->data[k][k])-0.0) < 1e-6){
+			return x=1;
+		}
             double wsp = (mat->data[w][k])/(mat->data[k][k]);
             for(int j = k; j < mat->c; j++)
                 mat->data[w][j] -= (mat->data[k][j])*wsp;
@@ -32,6 +36,6 @@ int eliminate(Matrix *mat, Matrix *b){
         }
     }    
  
-		return 0;
+		return x;
 }
 
